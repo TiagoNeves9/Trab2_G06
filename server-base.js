2e762195ec3060446bf797f873d0ccc36a95009a
Tiago Neves <tiagonevesx9@gmail.com>
2023-11-17 12:55:26 +0000

    Start

---
+++ server-base.js
@@ -0,0 +1,9 @@
+// Built-in HTTPS support
+const https = require("https");
+// Handling GET request (npm install express)
+const express = require("express");
+// Load of files from the local file system
+var fs = require('fs');
+
+const PORT = 8080;
+const app = express();
