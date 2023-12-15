a3b07158864bc885bb62b5bb9321cbc01a27a377
Tiago Neves <tiagonevesx9@gmail.com>
2023-12-15 00:10:57 +0000

    v1.1

--- Ex7/WebApp.js
+++ Ex7/WebApp.js
@@ -37,6 +37,7 @@ const API_KEY = "AIzaSyCl-eDTbeEXUjUmQw89kfob8INT40z9FbI";
 const CALLBACK = "callback";
 const CALLBACKGITHUB = "callbackgithub";
 
+let githubToken;
 
 // HOMEPAGE. Get request for resource
 app.get("/", async function (req, res) {
@@ -62,7 +63,8 @@ app.get("/", async function (req, res) {
             '<br> <a href = /login> Login with your Google Account </a>' +
             '<br><br> <a href=/allTasks> View your task lists </a>' +
             '<br><br> <a href=/githubmilestonesform> Obtain your github milestones </a>'+
-            '<br><br> <a href=/githublogin> Github Login </a>'
+            '<br><br> <a href=/githublogin> Github Login </a>' +
+            '<br><br> <a href = /logout> Logout </a>'
         );
     }
 });
@@ -84,61 +86,6 @@ app.get('/login', (req, res) => {
 
         // redirect uri used to register RP
         + 'redirect_uri=http://localhost:3001/' + CALLBACK);
-});
-
-app.get('/githublogin', (req, res) => {
-    res.redirect(302,
-        // authorization endpoint
-        'https://github.com/login/oauth/authorize?'
-
-        // client id
-        + 'client_id=' + GITHUB_CLIENT_ID + '&'
-
-        // scope
-        + 'scope=repo&' +
-
-        //callback url
-        + 'redirect_uri=http://localhost:3001/' + CALLBACKGITHUB);
-});
-
-let githubToken 
-
-// CALLBACK GITHUB
-app.get('/' + CALLBACKGITHUB, (req, res) => {
-    console.log('making request to token endpoint github');
-    // content-type: application/x-www-form-urlencoded (URL-Encoded Forms)
-    const form = new FormData();
-    form.append('client_id', GITHUB_CLIENT_ID);
-    form.append('client_secret', GITHUB_CLIENT_SECRET);
-    form.append('code', req.query.code);
-    form.append('redirect_uri', 'http://localhost:3001/' + CALLBACKGITHUB);
-    //console.log(form);
-
-    axios.post(
-        // token endpoint
-        'https://github.com/login/oauth/access_token',
-        form,
-        { headers: form.getHeaders() }
-    ).then(function (response) {
-        githubToken = response.data.split("&")[0].split("=")[1];
-        console.log(githubToken);
-        res.redirect("/");
-    })
-});
-
-app.get('/githubuser', async (req, res) => {
-    const octokit = new Octokit({
-        auth: githubToken
-    });
-
-    const user = await octokit.request('GET /user', {
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-      });
-
-    console.log(user);
-    res.redirect('/');  
 });
 
 // CALLBACK
@@ -189,6 +136,57 @@ app.get('/' + CALLBACK, (req, res) => {
             res.send();
         });
 });
+
+//Logout
+app.get('/logout', (req, res) => {
+    // Clear the cookie or session used for authentication
+    res.clearCookie('EmailCookie'); // Change 'EmailCookie' to your cookie name if needed
+    res.clearCookie('AccessTokenCookie'); // Change 'AccessTokenCookie' to your cookie name if needed
+
+    // Redirect to the home page or any other desired page after logout
+    res.redirect('/');
+});
+
+//github login
+app.get('/githublogin', (req, res) => {
+    res.redirect(302,
+        // authorization endpoint
+        'https://github.com/login/oauth/authorize?'
+
+        // client id
+        + 'client_id=' + GITHUB_CLIENT_ID + '&'
+
+        // scope
+        + 'scope=repo&' +
+
+        //callback url
+        + 'redirect_uri=http://localhost:3001/' + CALLBACKGITHUB);
+});
+
+// CALLBACK GITHUB
+app.get('/' + CALLBACKGITHUB, (req, res) => {
+    console.log('making request to token endpoint github');
+    // content-type: application/x-www-form-urlencoded (URL-Encoded Forms)
+    const form = new FormData();
+    form.append('client_id', GITHUB_CLIENT_ID);
+    form.append('client_secret', GITHUB_CLIENT_SECRET);
+    form.append('code', req.query.code);
+    form.append('redirect_uri', 'http://localhost:3001/' + CALLBACKGITHUB);
+    //console.log(form);
+
+    axios.post(
+        // token endpoint
+        'https://github.com/login/oauth/access_token',
+        form,
+        { headers: form.getHeaders() }
+    ).then(function (response) {
+        githubToken = response.data.split("&")[0].split("=")[1];
+        console.log(githubToken);
+        res.redirect("/");
+    })
+});
+
+
 
 async function getRole(req, res) {
     const e = await casbin.newEnforcer(__dirname + '/rbac/rbac_model.conf', __dirname + '/rbac/rbac_policy_app.csv');
@@ -283,10 +281,16 @@ app.post('/taskList/add/:id', (req, res) => {
 
 //Form to select the owner and repo to obtain the milestones
 app.get('/githubmilestonesform', (req, res) => {
-    res.render('repoMilestoneForm', { owner: req.params.owner, repo : req.params.repo, repoMilestoneForm: true });
+    if (Object.keys(req.cookies).length != 0) {
+        res.render('repoMilestoneForm', { owner: req.params.owner, repo : req.params.repo, repoMilestoneForm: true });
+    } else {
+        console.log("Tem de estar logado para entrar no taskList add");
+        res.redirect("/login");
+    }
 });
 
 async function milestones(req,res){
+    
     const lists = await axios.get(
         `https://tasks.googleapis.com/tasks/v1/users/@me/lists?access_token=${req.cookies.AccessTokenCookie}&key=${API_KEY}`,
         { Authorization: 'Bearer ' + req.cookies.AccessTokenCookie }
